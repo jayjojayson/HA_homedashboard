22245379bdf22df638d2228549ab7acb965a9718
jayjojayson <2683358+jayjojayson@users.noreply.github.com>
2026-01-13 16:28:30 +0100

    v.1.0.6

---
+++ dist/HA_homedashboard/HA-homedashboard-info.js
@@ -0,0 +1,18 @@
+/*
+ * DUMMY FILE FOR HACS COMPATIBILITY
+ * =================================
+ *
+ * Dieses Repository stellt hauptsächlich YAML-Konfigurationsdateien und Assets bereit,
+ * keine ausführbare Lovelace-Karte (Custom Card).
+ *
+ * Diese Datei dient als notwendiger Einstiegspunkt. Sie enthält keine Logik,
+ * sondern schreibt lediglich eine kleine, formatierte Bestätigungsmeldung in die
+ * Browser-Entwicklerkonsole (F12), um anzuzeigen, dass das Paket geladen wurde,
+ * ohne Fehler zu verursachen.
+ */
+
+console.info(
+    "%c HA_homedashboard %c Assets erfolgreich geladen ",
+    "color: white; background: #03a9f4; font-weight: 700; padding: 2px 5px; border-radius: 3px 0 0 3px;",
+    "color: #03a9f4; background: white; font-weight: 700; padding: 2px 5px; border: 1px solid #03a9f4; border-radius: 0 3px 3px 0;"
+);
